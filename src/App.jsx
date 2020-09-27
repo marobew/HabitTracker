0aa8541e21f1143d1ce104d0adfb436b61ebcb5b
marobew <boramwe.dev@gmail.com>
2020-09-27 16:48:16 +0900

    habit, habits 컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Habits from './components/Habits';
+
+class App extends Component {
+  render() {
+    return (
+      <Habits />
+    );
+  }
+}
+
+export default App;
